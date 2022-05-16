c32f57b64b114dabd06d6768b45596e2be87a107
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-16 15:05:27 -0700

    homepage logic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Login from "./Login.js";
 import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null)
+
   useEffect(() => {
-      const token = getTokenFromUrl();
+      const hash = getTokenFromUrl();
+      window.location.hash="";
+      
+      const _token = hash.access_token
+
+      if (_token) {
+        setToken(_token)
+      }
       console.log("I have it",token)
   }, []);
 
   return (
     <div className="App">
-      <Login/>
+      {
+        token ? <h1>I am logged in</h1> : <Login/>
+      }
     </div>
   );
 }
