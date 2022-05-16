11eb9886aebd5773013451be7a4ab788c263e342
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-16 13:54:11 -0700

    login component

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,7 @@ function Login() {
   return (
     <div>
         <h1>This Works</h1>
-        
+        <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
     </div>
   )
 }
