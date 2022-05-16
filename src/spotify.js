c698491e9a626d30bf177bf3e86e87405bd7ed7e
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-16 14:14:21 -0700

    login endpoints

--- src/spotify.js
+++ src/spotify.js
@@ -4,4 +4,13 @@
 export const authEndPoint = 
 "https://accounts.spotify.com/authorize/";
 
-const redirectUri = "https://localhost:3000/"
+const redirectUri = "https://localhost:3000/";
+const clientId = "d6daa6d0cc0240e6b8b7c6638a56522a";
+
+const scopes = [
+    "user-modify-playback-state",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-read-currently-playing",
+];
