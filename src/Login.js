74c701aaa15e81dc7c7b8004f19c5d7027ed22dd
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-16 13:55:04 -0700

    login component

--- src/Login.js
+++ src/Login.js
@@ -3,8 +3,8 @@ import './Login.css'
 function Login() {
   return (
     <div>
-        <h1>This Works</h1>
         <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
+        <button>LOGIN WITH SPOTIFY</button>
     </div>
   )
 }
