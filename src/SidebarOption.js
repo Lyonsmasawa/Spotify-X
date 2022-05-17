a207b789acaf7509f89303df9cd1bd84cfae217d
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-17 03:44:51 -0700

    build sidebar options

--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -4,6 +4,7 @@ import './SidebarOptions.css'
 function SidebarOption({title, Icon}) {
   return (
     <div className="sidebarOption">
+        { Icon && <Icon className="sidebarOption__icon"/>}
        { Icon ? <h4>{title}</h4> : <p>{title}</p> }
     </div>
   )
