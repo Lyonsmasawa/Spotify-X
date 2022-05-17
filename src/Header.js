a8778cd539e382e79aced30f1ef804343c53adef
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-17 07:20:23 -0700

    Header component design

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import Search from '@material-ui/icons/Search';
+import { Avatar } from '@material-ui/core';
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+        <div className="header__left">
+            <Search />
+            <input type="text"
+            placeholder='Search for Artists, Songs,'
+            />
+        </div>
+        <div className="header__right">
+            <Avatar />
+            <h4>Lyons Albert</h4>
+        </div>
+    </div>
   )
 }
 
