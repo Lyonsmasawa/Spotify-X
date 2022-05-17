80cbb4245f4bb65e51c119a982f4bae144f4ace1
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-17 07:28:42 -0700

    get user data

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './Header.css';
 import Search from '@material-ui/icons/Search';
 import { Avatar } from '@material-ui/core';
+import { useDataLayerValue } from './DataLayer';
 
 function Header() {
+
+    const [{ user }, dispatch] = useDataLayerValue();
+
   return (
     <div className='header'>
         <div className="header__left">
