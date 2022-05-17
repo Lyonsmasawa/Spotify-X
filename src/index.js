17948c30eedd80c0400661917e97aebe6d12939b
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-17 01:21:35 -0700

    reducer

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { initialState } from './reducer';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DataLayer initialState reducer>
+    <DataLayer initialState={initialState} reducer>
       <App />
     </DataLayer>
   </React.StrictMode>
