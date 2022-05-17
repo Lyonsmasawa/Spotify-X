954d522fa3be60d6b6d29f1686f7e4ce25b9ada9
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-17 01:19:08 -0700

    react context-api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { DataLayer } from './DataLayer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
