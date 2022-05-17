7ea6ec924ba7b5500c0befa6af9f8e59e40905f5
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-17 01:27:36 -0700

    reducer

--- src/reducer.js
+++ src/reducer.js
@@ -4,3 +4,19 @@ export const initialState = {
     playing: false,
     item: null,
 }
+
+const reducer = (state, action) => {
+    console.log(action)
+    // Action has a (type,  [payload])
+    switch(action.type) {
+        case 'SET_USER':
+            return {
+                ...state, //keep whatever was in the state prevent overwriting.
+                user: action.user // update user slice 
+            }
+        default:
+            return state;
+    }
+}
+
+export default reducer
