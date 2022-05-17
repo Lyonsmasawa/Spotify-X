6d6bca6614306ce85cfa109ffcf54f12306a04e3
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-17 07:11:04 -0700

    create Header component

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Body.css'
+import Header from './Header'
 
 function Body() {
   return (
     <div className="body">
-      
+      <Header />
     </div>
   ) 
 } 
