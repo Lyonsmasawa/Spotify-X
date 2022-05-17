5289232346af2b8b273fb88ad6556f9343ad131f
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-17 07:41:44 -0700

    build discover section

--- src/Body.js
+++ src/Body.js
@@ -6,6 +6,14 @@ function Body({ spotify }) {
   return (
     <div className="body">
       <Header spotify={spotify}/>
+      <div className="body__info">
+          <img src="https://i.scdn.co/image/ab6775700000ee85b254071f06759aa4a4f6398b" alt="" srcset="" />
+          <div className="body__infoText">
+            <strong>PLAYLIST</strong>
+            <h2>Discover Weekly</h2>
+            <p>description...</p>
+          </div>
+      </div>
     </div>
   ) 
 } 
