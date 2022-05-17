0aece7c0800a63d6cd5271fd94ba03ddfaf4b6d9
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-17 04:33:44 -0700

    set user playlists

--- src/reducer.js
+++ src/reducer.js
@@ -21,11 +21,7 @@ const reducer = (state, action) => {
                 token: action.token
             }
         
-        case 'SET_PLAYLISTS':
-            return {
-                ...state,
-                playlists: action.playlists,
-            }
+        
         default:
             return state;
     }
